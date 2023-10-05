d54504ab67ada99d3135d5f359a6ff7fe34a91e6
Abdullah Mohammed <djangodeveloper6@gmail.com>
2023-10-05 16:33:01 +0300

    add propery-details.html page

--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('property/', include('property.urls', namespace='property')),
     path('', include('pages.urls', namespace='pages')), 
     path('maps/', include('maps.urls', namespace='maps')), 
 ]
--- property/views.py
+++ property/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def property_details(request): 
+    context=  {} 
+    return render(request, 'property/property-details.html', context ) 
